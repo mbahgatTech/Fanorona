ab4e1975969294b9d5b8020cce4505aa938a6de7
mbahgatTech <mazenbahgat@outlook.com>
2024-11-05 16:10:34 -0500

    model boiler plate

--- src/Model/Board.rb
+++ src/Model/Board.rb
@@ -1,5 +1,50 @@
 class Board
   def initialize
+    @board = nil
+    @last_board = nil
+  end
+
+  def display_board
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def get_last_board
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def move_piece(from_row, from_col, to_row, to_col)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def setup_game_board
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def how_many_colour(colour)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_diagonals(row, col)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_horizontals(row, col)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_verticals(row, col)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_empty_space(row, col)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def revert_move
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_move_type(from_row, from_col, to_row, to_col)
     FanoronaLogger.log_error('Not Implemented')
   end
 end
--- src/Model/Player.rb
+++ src/Model/Player.rb
@@ -1,5 +1,36 @@
 class Player
   def initialize
+    colour = nil
+    turn = nil
+    pieces_captured = nil
+    moves_made_on_turn = nil
+  end
+
+  def checkColour
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def make_move(from_row, from_col, to_row, to_col, board)
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def capture_piece
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def revert_move
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def end_turn
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def flip_turn
+    FanoronaLogger.log_error('Not Implemented')
+  end
+
+  def check_moves_made
     FanoronaLogger.log_error('Not Implemented')
   end
 end
